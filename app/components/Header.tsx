51477056c09678aa0e0d68c4a183014ac4281aab
Nara Ratanakkithya <narakithya@gmail.com>
2026-02-18 12:02:54 +0700

    fix error typo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -11,7 +11,11 @@ const Header = () => {
         whileInView={{ scale: 1 }}
         transition={{ duration: 0.8, type: "spring", stiffness: 100 }}
       >
-        <Image src={assets.profile_round} alt="" className="rounded-full w-32" />
+        <Image
+          src={assets.profile_round}
+          alt=""
+          className="rounded-full w-32"
+        />
       </motion.div>
       <motion.h3
         initial={{ y: -20, opacity: 0 }}
@@ -56,7 +60,7 @@ const Header = () => {
           initial={{ y: 30, opacity: 0 }}
           whileInView={{ y: 0, opacity: 1 }}
           transition={{ duration: 0.6, delay: 1 }}
-          href="/sample-resume.pdf"
+          href="/CV.pdf"
           download
           className="flex items-center gap-2 px-10 py-3 border rounded-full border-gray-500 dark:text-black bg-white"
         >
